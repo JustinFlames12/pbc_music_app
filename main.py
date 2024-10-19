1a21521ae9a950c71d483345ea72b71215e4f9fb
Justin <48168389+JustinFlames12@users.noreply.github.com>
2024-10-19 14:42:27 -0400

    Added extra logic (play, stop, choose) to app

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import kivy libraries
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -5,6 +6,24 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.dropdown import DropDown
 from kivy.core.window import Window
 from kivy.uix.actionbar import ActionBar
+from kivy.core.audio import SoundLoader
+from kivy.utils import platform
+
+# Import python libraries
+import os
+
+# Run android logic for requesting permissions (only applicable when running app on android)
+try: 
+    from android.permissions import request_permissions, Permission
+    from android.storage import primary_external_storage_path
+    request_permissions([
+        Permission.READ_MEDIA_AUDIO,
+        # Permission.INTERNET,
+        Permission.WRITE_EXTERNAL_STORAGE, 
+        Permission.READ_EXTERNAL_STORAGE
+    ])
+except Exception as e:
+    print(f'INFO: Skipping android library since app is not running on android\n{e}')
 
 class MusicApp(App):
     def build(self):
@@ -19,23 +38,35 @@ class MusicApp(App):
         self.current_song_prompt = Label(text = f"Current song:\n{self.current_song}", color = (0.2, 0.2, 0.2, 1))
         self.first_layout = GridLayout(cols = 2)
         self.play_button = Button(text = 'Play', background_color = button_clr, on_release = self.on_off_cycle)
+        self.play_button.disabled = True
 
         # create a dropdown with 10 buttons
         self.dropdown = DropDown()
-        for index in range(10):
-            # When adding widgets, we need to specify the height manually
-            # (disabling the size_hint_y) so the dropdown can calculate
-            # the area it needs.
-
-            self.song_btn = Button(text='Song %d' % index, size_hint_y=None, height=30)
-            
-            # for each button, attach a callback that will call the select() method
-            # on the dropdown. We'll pass the text of the button as the data of the
-            # selection.
-            self.song_btn.bind(on_release=lambda btn: self.change_song(btn, btn.text))
-
-            # then add the button inside the dropdown
-            self.dropdown.add_widget(self.song_btn)
+        # for index in range(10):
+        if platform != 'android':
+            for song in os.listdir('.\soundtrack'):
+                # When adding widgets, we need to specify the height manually
+                # (disabling the size_hint_y) so the dropdown can calculate
+                # the area it needs.
+                self.song_btn = Button(text=song, size_hint_y=None, height=30)
+                # for each button, attach a callback that will call the select() method
+                # on the dropdown. We'll pass the text of the button as the data of the
+                # selection.
+                self.song_btn.bind(on_release=lambda btn: self.change_song(btn, btn.text))
+                # then add the button inside the dropdown
+                self.dropdown.add_widget(self.song_btn)
+        else:
+            for song in os.listdir('soundtrack'):
+                # When adding widgets, we need to specify the height manually
+                # (disabling the size_hint_y) so the dropdown can calculate
+                # the area it needs.
+                self.song_btn = Button(text=song, size_hint_y=None, height=30)
+                # for each button, attach a callback that will call the select() method
+                # on the dropdown. We'll pass the text of the button as the data of the
+                # selection.
+                self.song_btn.bind(on_release=lambda btn: self.change_song(btn, btn.text))
+                # then add the button inside the dropdown
+                self.dropdown.add_widget(self.song_btn)
 
         # create a big main button
         self.mainbutton = Button(text='All songs', size_hint=(1, None), background_color = button_clr)
@@ -49,9 +80,8 @@ class MusicApp(App):
         # one last thing, listen for the selection in the dropdown list and
         # assign the data to the button text.
         self.dropdown.bind(on_select=lambda instance, x: setattr(self.mainbutton, 'text', x))
-
         self.stop_button = Button(text='Stop', background_color = button_clr, on_release = self.on_off_cycle)
-
+        self.stop_button.disabled = True
         self.first_layout.add_widget(self.current_song_prompt)
         self.first_layout.add_widget(self.play_button)
         self.first_layout.add_widget(self.mainbutton)
@@ -63,17 +93,37 @@ class MusicApp(App):
 
     def on_off_cycle(self, instance):
         if self.play_button.disabled == True:
+            self.sound.stop()
             self.stop_button.disabled = True
             self.play_button.disabled = False
         else:
+            self.sound.play()
             self.play_button.disabled = True
             self.stop_button.disabled = False
 
     def change_song(self, instance, text):
+        try:
+            self.sound.stop()
+        except Exception as e:
+            print(f'There was no playing song to stop. Error Message: {e}')
         self.current_song_prompt.text = f"Current song:\n{text}"
         self.dropdown.select(text)
-        if self.play_button.disabled == True:
-            self.on_off_cycle(self.stop_button)
+        # if (self.play_button.disabled == True and self.stop_button.disabled == False):
+        #     self.on_off_cycle(self.stop_button)
+        #     # self.sound.stop()
+        # elif (self.play_button.disabled == True and self.stop_button.disabled == True):
+        #     self.stop_button.disabled = False
+        # elif (self.play_button.disabled == False and self.stop_button.disabled == True):
+        #     self.stop_button.disabled = False
+        #     self.play_button.disabled = True
+        self.stop_button.disabled = False
+        self.play_button.disabled = True
+        if platform != 'android':
+            self.sound = SoundLoader.load(f".\soundtrack\{text}")
+            self.sound.play()
+        else:
+            self.sound = SoundLoader.load(f"soundtrack/{text}")
+            self.sound.play()
 
 if __name__ == '__main__':
     MusicApp().run()
